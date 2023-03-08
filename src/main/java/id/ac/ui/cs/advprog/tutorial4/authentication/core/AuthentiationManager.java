7564a3efa293df5ee93c5cae62b03afd390387f5
eugeniusms <eugeniusms@gmail.com>
2023-03-08 13:46:14 +0700

    #7 Authentication Controller AuthenticationController Done

--- src/main/java/id/ac/ui/cs/advprog/tutorial4/authentication/core/AuthentiationManager.java
+++ src/main/java/id/ac/ui/cs/advprog/tutorial4/authentication/core/AuthentiationManager.java
@@ -7,36 +7,56 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AuthentiationManager {
-    // TODO Buat private constructor
-
-    // TODO lengkapi method getInstance
-    // boleh mengedit static attribute jika diperlukan
     private static AuthentiationManager instance;
+
+    // TODO : DONE
+    // Private constructor
+    private AuthentiationManager() {};
+
+    // TODO : DONE
+    // Singleton pattern
     public static AuthentiationManager getInstance(){
-        return null;
+        if (instance == null) {
+            instance = new AuthentiationManager();
+        }
+        return instance;
     }
-    
-    
+
     private Map<String, String> tokenToUsernameMapping = new HashMap<>();
     
     
-    // TODO
+    // TODO: DONE
     //  lengkapi method-method di bawah ini, serta jangan lupa mengimplementasi supaya
     //  method-method tersebut throw exception yang tepat.
     //  Jangan lupa juga untuk mempelajari kegunaan @ControllerAdvice
     //  dan peran dari masing-masing class pada bagian tutorial ini
     
     public void registerNewToken(String token, String username){
-        // TODO
+        // TODO: DONE
+        // Jika token sudah ada di dalam tokenToUsernameMapping, throw InvalidTokenException
+        if (tokenToUsernameMapping.containsKey(token)) {
+            throw new UsernameAlreadyLoggedIn();
+        }
+        // Jika token tidak ada di dalam tokenToUsernameMapping, tambahkan token dan username ke dalam tokenToUsernameMapping
+        tokenToUsernameMapping.put(token, username);
     }
     
     public void removeToken(String token){
-        // TODO
+        // TODO: DONE
+        // Jika token tidak ada di dalam tokenToUsernameMapping, throw InvalidTokenException
+        if (!tokenToUsernameMapping.containsKey(token)) {
+            throw new InvalidTokenException();
+        }
+        tokenToUsernameMapping.remove(token);
     }
     
     public String getUsername(String token){
-        // TODO
-        return null;
+        // TODO: DONE
+        // Jika token tidak ada di dalam tokenToUsernameMapping, throw InvalidTokenException
+        if (!tokenToUsernameMapping.containsKey(token)) {
+            throw new InvalidTokenException();
+        }
+        return tokenToUsernameMapping.get(token);
     }
     
 }
